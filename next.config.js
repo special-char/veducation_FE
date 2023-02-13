4a6f97f8a37b2603fd8c9bed782e78cf822b02b8
Yagnesh Modh <yagnesh.modh@gmail.com>
2023-02-13 11:59:04 +0530

    init commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
+  swcMinify: true,
   experimental: {
+    // Required:
     appDir: true,
   },
-}
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+  images: {
+    domains: [
+      "assets.website-files.com",
+      "ik.imagekit.io",
+      "website-files.com",
+    ],
+    loader: "custom",
+    loaderFile: "./src/lib/imageLoader.js",
+    formats: ["image/avif", "image/webp"],
+  },
+};
+
+module.exports = nextConfig;
