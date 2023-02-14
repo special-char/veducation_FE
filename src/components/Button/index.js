e1bc90dc7fd3fbd76a30206daa579420d47305bc
Hemang Mesariya <hemangmesariya009@gmail.com>
2023-02-14 13:07:36 +0530

    button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,24 @@
+import clsx from "clsx";
+import React from "react";
+
+const Button = ({ ...props }) => {
+  return (
+    <button
+      className={clsx("btn", {
+        "btn--primary": props.variant === "primary",
+        "btn--primary3": props.variant === "primary2",
+        //"btn--secondary": props.variant === "secondary",
+        // "btn--secondary2": props.variant === "count",
+        "btn--small": props.size === "small",
+        "btn--large": props.size === "large",
+        "btn--count": props.size === "count",
+        "btn--round": props.isRound,
+        [props.className || ""]: !!props.className,
+      })}
+    >
+      {props.btnText}
+    </button>
+  );
+};
+
+export default Button;
