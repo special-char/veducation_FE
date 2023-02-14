f7c9dc7c499c69fceed3e77bdfb199702218f93e
ahir yogesh <ahiryogesh721@gmail.com>
2023-02-14 05:17:02 -0800

    stuck on icon

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import "../footer/style.module.css"
+import React from "react";
+import styles from "./style.module.css";
+
+const data = [
+  {
+    name: "Home",
+  },
+  {
+    name: "Library",
+  },
+  {
+    name: "Courses",
+  },
+  {
+    name: "Yodha Store",
+  },
+  {
+    name: "More",
+  },
+];
 
 const Footer = () => {
   return (
-    <div className='footer'>
-        footer
+    <div className={styles.footer}>
+      {data.map((x) => (
+        <a className={styles.footer__item}>
+          <img src="" />
+          <p>{x.name}</p>
+        </a>
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default Footer;
