00117c35adb2e7dd95164f7e5335412a2f2c929c
Hemang Mesariya <hemangmesariya009@gmail.com>
2023-02-15 16:31:08 +0530

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,55 @@ import Navbar from "@/components/Navbar/navbar";
 import ProductItem from "@/components/productItem";
 import Reviews from "@/components/Reviews";
 import SocialLinks from "@/components/SocialLinks";
+import Book from "public/book.png";
+
+const data = [
+  {
+    src: Book,
+    name: "B.O.S.S : Basics of Sanatan Sanskriti",
+    author: "Prateeik Prajapati",
+  },
+  {
+    src: Book,
+    name: "B.O.S.S : Basics of Sanatan Sanskriti",
+    author: "Prateeik Prajapati",
+  },
+  {
+    src: Book,
+    name: "B.O.S.S : Basics of Sanatan Sanskriti",
+    author: "Prateeik Prajapati",
+  },
+  {
+    src: Book,
+    name: "B.O.S.S : Basics of Sanatan Sanskriti",
+    author: "Prateeik Prajapati",
+  },
+];
 
 export default function Home() {
   return (
-    <section>
-      <div className="h-screen">ProductItem</div>
-      <Navbar />
+    <section className="">
+      <div className="h-screen ">
+        <SocialLinks />
+
+        <div className="flex overflow-x-scroll gap-1">
+          {data.map((val) => {
+            return (
+              <ProductItem
+                key={val.name}
+                img={val.src}
+                title={val.name}
+                author={val.author}
+                rate={4}
+                count={21}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <div>
+        <Navbar />
+      </div>
     </section>
   );
 }
