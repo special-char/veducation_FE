c358b7a3dccb66cfec4a36f4d924f046ea01c6ec
ahir yogesh <ahiryogesh721@gmail.com>
2023-02-15 02:17:53 -0800

    done allm

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./style.module.css";
 import Vector from "public/img/Vector.svg";
 import Books from "public/img/book-3.svg";
-import Courses from "public/img/courses.svg";
+import Courses from "public/img/Group.svg";
 import Wepon from "public/img/wepon.svg";
 import More from "public/img/more.svg";
 
@@ -33,8 +33,8 @@ const Footer = () => {
   return (
     <div className={styles.footer}>
       {data.map((x) => (
-        <a className="footer__item" href="#">
-          <span className="span1">{x.svg}</span>
+        <a className={styles.footer__iteam} href="#">
+          <span className={styles.span1}>{x.svg}</span>
           <p className="iteamn">{x.name}</p>
         </a>
       ))}
