21855af12477d0862b3a6bca6b7911090bd94624
harzh307 <harzh.patel@gmail.com>
2023-02-16 18:17:19 +0530

    modal commited

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,16 @@
 import ProductItem from "@/components/productItem";
 import Coursecard from "@/components/Coursecard/coursecard";
 import SocialLinks from "@/components/SocialLinks";
+import SignIn from "@/containers/SignIn";
 
 export default function Home() {
+  function onClick() {}
   return (
     <div className="h-full">
-      <SocialLinks />
-      <ProductItem />
-      <Coursecard />
+      <SocialLinks onClick={onClick} />
+      {/* <ProductItem />
+      <Coursecard /> */}
+      <SignIn />
     </div>
   );
 }
--- src/containers/SignIn/index.js
+++ src/containers/SignIn/index.js
@@ -3,7 +3,9 @@ import Button from "@/components/Button";
 import Modal from "@/components/Modal";
 import Link from "next/link";
 import React, { useState } from "react";
-
+import Veducation from "public/VEDUCATION.svg";
+import Icon from "public/veducationIcon.svg";
+import Input from "@/components/InputComponent";
 const SignIn = () => {
   const [open, setOpen] = useState(false);
   function toggleModal() {
@@ -15,8 +17,13 @@ const SignIn = () => {
       <Button onClick={toggleModal} as={Link} href="">
         Open
       </Button>
-      <Modal open={open} onClose={toggleModal}>
-        SignIn
+      <Modal open={open} onClose={toggleModal} className="flex-1 pt-3 px-3">
+        <span className="flex justify-center items-center">
+          <Icon />
+          <Veducation />
+        </span>
+        <p className="text-[23px] font-bold text-background2">Sign In</p>
+        <Input label="name" />
       </Modal>
     </div>
   );
