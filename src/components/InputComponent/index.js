5b982f559ee68fff2f0b50232682da56a24dc7e0
Hemang Mesariya <hemangmesariya009@gmail.com>
2023-02-17 11:17:15 +0530

    sign in page ui

--- src/components/InputComponent/index.js
+++ src/components/InputComponent/index.js
@@ -1,16 +1,16 @@
 import React from "react";
-import "../InputComponent/input.module.css";
+import styles from "../InputComponent/input.module.css";
 
 const Input = ({ ...props }) => {
   return (
-    <div className="section ">
+    <div className={styles.section}>
       <label className="body">{props.label}</label>{" "}
       <input
         type={props.type}
         placeholder={props.placeholder}
         id={props.id}
         {...props}
-        className="inbox"
+        className="inbox w-full p-2 placeholder-secondary border border-[#F5D9B1] rounded-md px-2 text-base"
       />
     </div>
   );
