79f597a6fd2e6e26106a32f5aae756e620d03d17
Hemang Mesariya <hemangmesariya009@gmail.com>
2023-02-17 16:23:09 +0530

    otp-varification screen ui

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,14 @@ import Navbar from "@/components/Navbar/navbar";
 import ProductItem from "@/components/productItem";
 import SocialLinks from "@/components/SocialLinks";
 import Btnnavigator from "@/components/Buttonnavigator";
+import SignIn from "@/containers/SignIn";
+import Varification from "@/containers/otpVarification";
 
 export default function Home() {
   function onClick() {}
   return (
-    <div className="h-screen px-container">
-      <Coursecard />
+    <div className="h-screen ">
+      <Varification />
     </div>
   );
 }
