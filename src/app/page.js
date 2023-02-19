272514becbe64a562e656e5de53ba9fe0c991717
Hemang Mesariya <hemangmesariya009@gmail.com>
2023-02-19 14:25:16 +0530

    donation card ui

--- src/app/page.js
+++ src/app/page.js
@@ -6,12 +6,51 @@ import SocialLinks from "@/components/SocialLinks";
 import Btnnavigator from "@/components/Buttonnavigator";
 import SignIn from "@/containers/SignIn";
 import Varification from "@/containers/otpVarification";
+import Productlist from "@/components/productItem";
+import Book from "public/book.png";
+import DonationCard2 from "@/components/DonationCard";
+import DonationCard from "@/components/DonationCard";
+
+const data = [
+  {
+    img: Book,
+    title: "B.O.S.S : Basics of Sanatan Sanskriti",
+    author: "Prateeik Prajapati",
+    rate: 3,
+    count: 20,
+  },
+  {
+    img: Book,
+    title: "B.O.S.S : Basics of Sanatan Sanskriti",
+    author: "Prateeik Prajapati",
+    rate: 3,
+    count: 20,
+  },
+  {
+    img: Book,
+    title: "B.O.S.S : Basics of Sanatan Sanskriti",
+    author: "Prateeik Prajapati",
+    rate: 3,
+    count: 20,
+  },
+  {
+    img: Book,
+    title: "B.O.S.S : Basics of Sanatan Sanskriti",
+    author: "Prateeik Prajapati",
+    rate: 3,
+    count: 20,
+  },
+];
 
 export default function Home() {
   function onClick() {}
   return (
-    <div className="h-screen ">
-      <Varification />
+    <div className="h-full pb-44">
+      <SocialLinks />
+      <div className="px-container">
+        <Productlist productList={data} />
+        <DonationCard />
+      </div>
     </div>
   );
 }
