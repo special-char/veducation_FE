5402774bf027e4baaa5e0f33d6fde9cc3d07e874
Hemang Mesariya <hemangmesariya009@gmail.com>
2023-02-19 14:14:24 +0530

    donation card

--- src/app/page.js
+++ src/app/page.js
@@ -6,12 +6,52 @@ import SocialLinks from "@/components/SocialLinks";
 import Btnnavigator from "@/components/Buttonnavigator";
 import SignIn from "@/containers/SignIn";
 import Varification from "@/containers/otpVarification";
+import SignUpDetails from "@/containers/SignUpDetails";
+import Productlist from "@/components/productItem";
+import Book from "public/book.png";
+import DonationCard from "@/components/Donation/donationCard";
+import DonationCard2 from "@/components/DonationCard";
+
+const data = [
+  {
+    img: Book,
+    title: "B.O.S.S : Basics of Sanatan Sanskriti",
+    author: "Prateeik Prajapati",
+    rate: 3,
+    count: 20,
+  },
+  {
+    img: Book,
+    title: "B.O.S.S : Basics of Sanatan Sanskriti",
+    author: "Prateeik Prajapati",
+    rate: 3,
+    count: 20,
+  },
+  {
+    img: Book,
+    title: "B.O.S.S : Basics of Sanatan Sanskriti",
+    author: "Prateeik Prajapati",
+    rate: 3,
+    count: 20,
+  },
+  {
+    img: Book,
+    title: "B.O.S.S : Basics of Sanatan Sanskriti",
+    author: "Prateeik Prajapati",
+    rate: 3,
+    count: 20,
+  },
+];
 
 export default function Home() {
   function onClick() {}
   return (
-    <div className="h-screen ">
-      <Varification />
+    <div className="h-full pb-44 ">
+      <SocialLinks />
+      <div className="px-container">
+        <Productlist productList={data} />
+        <DonationCard2 />
+      </div>
     </div>
   );
 }
--- src/components/Donation/donationCard.jsx
+++ src/components/Donation/donationCard.jsx
@@ -9,20 +9,25 @@ const DonationCard = () => {
       <div className={styles.main__first}>
         <div className={styles.main__header}>
           <h1 className={styles.main__heading}>Donations</h1>
-          <a href="#View" className={styles.main__view} >
+          <a href="#View" className={styles.main__view}>
             View all
           </a>
         </div>
         <div className={styles.main__card}>
           <Image src={Imag1} alt="donation" className={styles.main__img} />
+
           <div className={styles.main__text}>
-            <h2 className={styles.main__helpUsTxt}>Help us spread <br /> Dharma</h2>
+            <h2 className={styles.main__helpUsTxt}>
+              Help us spread <br /> Dharma
+            </h2>
             <p className={styles.main__discription}>
               Help us in this mission of spreading this ancient knowledge all
               over the world. So that we can again make
               <span className={styles.main__span}>'The Golden Bird'</span>
             </p>
-            <button className={styles.main__button}>3 Ways you can help us..</button> 
+            <button className={styles.main__button}>
+              3 Ways you can help us
+            </button>
           </div>
         </div>
       </div>
