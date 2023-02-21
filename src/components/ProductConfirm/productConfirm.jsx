9337746da194f2336d1d098a57e34d26b7bc5527
Devang Prajapati <200103102134@silveroakuni.ac.in>
2023-02-21 14:32:31 +0530

    your Order page done

--- src/components/ProductConfirm/productConfirm.jsx
+++ src/components/ProductConfirm/productConfirm.jsx
@@ -8,65 +8,17 @@ import Minus from "public/icons/minus.svg";
 import Delete from "public/icons/delete.svg";
 import OrderdItem from "../oderedItem/orderedItem";
 
-const items = [
-  {
-    img: Tshirt,
-    item: "Veducation T Shirt - Mens Tshirt Pure Black",
-    price: "25.00",
-    quantity: "5",
-    arrivingDate: "23",
-    arrivingMonth: "oct",
-    deliveryDate: "24",
-    deliveryMonth: "oct",
-    year: "2025",
-  },
-  {
-    img: Tshirt,
-    item: "Veducation T Shirt - Mens Tshirt Pure Black",
-    price: "22.00",
-    quantity: "5",
-    arrivingDate: "23",
-    arrivingMonth: "oct",
-    deliveryDate: "24",
-    deliveryMonth: "oct",
-    year: "2025",
-  },
-  {
-    img: Tshirt,
-    item: "Veducation T Shirt - Mens Tshirt Pure Black",
-    price: "50.00",
-    quantity: "5",
-    arrivingDate: "23",
-    arrivingMonth: "oct",
-    deliveryDate: "24",
-    deliveryMonth: "oct",
-    year: "2025",
-  },
-  {
-    img: Tshirt,
-    item: "Veducation T Shirt - Mens Tshirt Pure Black",
-    price: "20.00",
-    quantity: "3",
-    arrivingDate: "23",
-    arrivingMonth: "oct",
-    deliveryDate: "24",
-    deliveryMonth: "oct",
-    year: "2025",
-  },
-  {
-    img: Tshirt,
-    item: "Veducation T Shirt - Mens Tshirt Pure Black",
-    price: "25.00",
-    quantity: "5",
-    arrivingDate: "23",
-    arrivingMonth: "oct",
-    deliveryDate: "24",
-    deliveryMonth: "oct",
-    year: "2025",
-  },
-];
 
-const ProductConfirm = () => {
+// const item={
+//   img:Tshirt,
+//   item:"Veducation T Shirt - Mens Tshirt Pure Black",
+//   price:"25.00",
+//   totalpcs:"3pcs",
+//   arriving:"12 june",
+//   deliveredOn:"13 june"
+// }
+
+const ProductConfirm = ({ image, title, price }) => {
   const [arriving, setArriving] = useState(false);
   const [count, setCount] = useState(1);
   const incrementCount = () => {
@@ -81,50 +33,48 @@ const ProductConfirm = () => {
 
   return (
     <div className={styles.main}>
-      {items.map((x, i) => (
-        <div className={styles.main__detail} key="items">
-          <div className="relative aspect-square min-h-[93px]   ">
-            <Image src={x.img} alt="item" fill className={styles.main__img} />
-          </div>
-          <div className={styles.main__desc}>
-            <h5 className={styles.main__name}>{x.item}</h5>
+      <div className={styles.main__detail} key="items">
+        <div className="relative aspect-square min-h-[93px]   ">
+          <Image src={image} alt="item" fill className={styles.main__img} />
+        </div>
+        <div className={styles.main__desc}>
+          <h5 className={styles.main__name}>{title}</h5>
+          {count > 0 && (
+            <p className={styles.main__price}>
+              $ {count * price}
+              {/* | {count} pcs */}
+            </p>
+          )}
+
+          <div>
             {count > 0 && (
-              <p className={styles.main__price}>
-                $ {count * x.price}
-                {/* | {count} pcs */}
-              </p>
+              <div className={styles.main__counter}>
+                <button
+                  onClick={decrementCount}
+                  className={styles.main__counterBtn}
+                >
+                  <Minus className={styles.main__counterSvg} />
+                </button>
+                <h5 className={styles.main__counterValue}>{count}</h5>
+                <button
+                  onClick={incrementCount}
+                  className={styles.main__counterBtn}
+                >
+                  <Plus className={styles.main__counterSvg} />
+                </button>
+
+                <button
+                  className={styles.main__dltBtn}
+                  onClick={() => setCount(0)}
+                >
+                  <Delete />
+                  Delete
+                </button>
+              </div>
             )}
-
-            <div>
-              {count > 0 && (
-                <div className={styles.main__counter}>
-                  <button
-                    onClick={decrementCount}
-                    className={styles.main__counterBtn}
-                  >
-                    <Minus className={styles.main__counterSvg} />
-                  </button>
-                  <h5 className={styles.main__counterValue}>{count}</h5>
-                  <button
-                    onClick={incrementCount}
-                    className={styles.main__counterBtn}
-                  >
-                    <Plus className={styles.main__counterSvg} />
-                  </button>
-
-                  <button
-                    className={styles.main__dltBtn}
-                    onClick={() => setCount(0)}
-                  >
-                    <Delete />
-                    Delete
-                  </button>
-                </div>
-              )}
-            </div>
           </div>
         </div>
-      ))}
+      </div>
     </div>
   );
 };
