e6cde8ce080427d809e9659ccc0f7ebf6ef5a255
Hemang Mesariya <hemangmesariya009@gmail.com>
2023-03-02 12:12:07 +0530

    about page api

--- src/components/About/AboutPage.jsx
+++ src/components/About/AboutPage.jsx
@@ -8,24 +8,27 @@ const details = {
   p1: "Learn about vedic science, history, philosophy & culture in details.With Prateeik Prajapati, who has been reading and researching collective psychologies of world civilisations since last 7 years.",
   p2: "Now he has decided to share all that invaluable knowledge with the world through his talks and sessions in schools, colleges, corporates, villages & online through Youtube & social media.",
   buttonname: "Tell Me More",
-  img: Img
+  img: Img,
 };
 
-const AboutPage = () => {
+const AboutPage = ({ data }) => {
+  console.log("AboutPage data:", data);
   return (
     <div className={styles.AboutPage}>
       <div className={styles.AboutPage__Img}>
         <Image src={details.img} alt="aboutpage" className="w-full" />
         <div className={styles.AboutPage__Content}>
-          <p className="pb-0 mb-0 text-secondary font-medium">Welcome to </p>
-          <p className={styles.AboutPage__span}>Veducation World</p>
+          <p className="pb-0 mb-0 text-secondary font-medium">
+            {data.attributes.title}
+          </p>
         </div>
       </div>
       <div className={styles.AboutPage__details}>
-        <p className={styles.AboutPage__details1}>{details.p1}</p>
-        <p className={styles.AboutPage__details1}>{details.p2}</p>
+        <p className={styles.AboutPage__details1}>
+          {data.attributes.description}
+        </p>
+
         <div className={styles.AboutPage__button}>
-          {" "}
           <Button
             variant="primary"
             size="large"
