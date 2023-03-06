e2c93a413f546e86763f78904d066db06341d6da
Hemang Mesariya <hemangmesariya009@gmail.com>
2023-03-06 11:54:33 +0530

    api integreted in courses

---
+++ src/lib/getCourseDetails.js
@@ -0,0 +1,11 @@
+const { default: axiosInstance } = require("./axiosInstance");
+
+export const getCourseDetails = async (id) => {
+  try {
+    const response = await axiosInstance.get(`/courses/${id}?populate=*`);
+    // setList(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
