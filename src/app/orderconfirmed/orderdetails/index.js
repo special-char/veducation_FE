a8ed94882282d369a30fd61125a0d0ef75da81ad
Hemang Mesariya <hemangmesariya009@gmail.com>
2023-03-10 12:31:03 +0530

    orderDetails ui according figma

--- src/app/orderconfirmed/orderdetails/index.js
+++ src/app/orderconfirmed/orderdetails/index.js
@@ -4,65 +4,35 @@ import Date from "public/icons/date.svg";
 import Total from "public/icons/total.svg";
 import Payment from "public/icons/payment.svg";
 import Styles from "./orderDetails.module.css";
-import Button from "../../../components/Button";
-import Link from "next/link";
-
-const details = {
-  orderCode: "#85764564674646",
-  date: "February 19, 2021",
-  total: "$1,345.00",
-  payment: "Cash on Delivery",
-};
 
 const OrderDetails = ({ orderCode, date, total, payment }) => {
+  const details = [
+    {
+      svg: <OrderCode />,
+      title: "OrderCode",
+      data: "#85764564674646",
+    },
+    { svg: <Date />, title: "Date", data: "February 19, 2021" },
+    { svg: <Total />, title: "Total", data: "$1,345.00" },
+    { svg: <Payment />, title: "Payment method", data: "Cash on Delivery" },
+  ];
   return (
     <div className={Styles.details}>
-      <div className={Styles.details__heading}>
-        <p className={Styles.details__hText}>Details</p>
-      </div>
+      <h5 className={Styles.details__heading}>Details</h5>
       <div className={Styles.details__detail}>
-        <div className={Styles.details__border}>
-          <div className={Styles.details__item}>
-            <OrderCode />
-            <p className={Styles.details__name}>Order code:</p>
-          </div>
-          <div className={Styles.details__value}>
-            <p className={details.details__Text}>{orderCode}</p>
-          </div>
-        </div>
-
-        <div className={Styles.details__detail}>
-          <div className={Styles.details__item}>
-            <Date />
-            <p className={Styles.details__name}>Date:</p>
-          </div>
-          <div className={Styles.details__value}>
-            <p className={details.item__Text}>{date}</p>
-          </div>
-        </div>
-
-        <div className={Styles.details__border}>
-          <div className={Styles.details__item}>
-            <Total />
-            <p className={Styles.details__name}>Total:</p>
-          </div>
-          <div className={Styles.details__value}>
-            <p className={details.details__Text}>{total}</p>
-          </div>
-        </div>
-
-        <div className={Styles.details__border2}>
-          <div className={Styles.details__item}>
-            <Payment />
-            <p className={Styles.details__name}>Payment method:</p>
-          </div>
-          <div className={Styles.details__value}>
-            <p className={details.details__iText}>{payment}</p>
-          </div>
-        </div>
+        {details.map((val, index) => {
+          return (
+            <div key={index} className={Styles.details__border}>
+              <div className={Styles.details__item}>
+                {val.svg}
+                <p className={Styles.details__name}>{val.title}:</p>
+              </div>
+              <p className={Styles.details__Text}>{val.data}</p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
 };
-
 export default OrderDetails;
