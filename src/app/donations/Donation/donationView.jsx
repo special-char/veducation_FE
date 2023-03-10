6a20d76ed4baf8c2da0d80385d7072e4f0151128
Hemang Mesariya <hemangmesariya009@gmail.com>
2023-03-10 18:40:07 +0530

    donationCard ui as per figma

--- src/app/donations/Donation/donationView.jsx
+++ src/app/donations/Donation/donationView.jsx
@@ -9,6 +9,8 @@ import Clock from "public/Clock.svg";
 import People from "public/icons/people.svg";
 import Progress from "./progressBar";
 import { getDonationsData } from "@/lib/getDonationsData";
+import Button from "@/components/Button";
+import Link from "next/link";
 
 const users = [
   {
@@ -42,17 +44,12 @@ const DonationView = async () => {
   const data = donations.data;
   return (
     <div className={styles.main} id="View">
-      <div className={styles.main__header}>
-        <h1 className={styles.main__heading}>Donations</h1>
-      </div>
-
+      <h5 className={styles.main__heading}>Donations</h5>
       {data.map((val, i) => {
         const progressValue = val.attributes?.raised
           ?.split("")
           ?.filter((x) => x !== ",")
           .join("");
-        // .filter((x) => x !== ",")
-        // .join("");
         const progressMax = val.attributes.raisedOutOf
           .split("")
           ?.filter((x) => x !== ",")
@@ -63,13 +60,14 @@ const DonationView = async () => {
             <div className={styles.main__donationdata}>
               <Image src={Imag2} className={styles.main__img} alt="donation" />
               {/* {x.img} */}
-              <h6 className={styles.main__about}>{val.attributes.title}</h6>
-              <p className={styles.main__value}>
+              <h5 className={styles.main__about}>{val.attributes.title}</h5>
+              <div className={styles.main__value}>
                 <span className={styles.main__span}>
                   ${val.attributes.raised}
-                </span>{" "}
-                raised out of ${val.attributes.raisedOutOf}
-              </p>
+                </span>
+                raised out of
+                <span className="text-base">${val.attributes.raisedOutOf}</span>
+              </div>
 
               {/* <p>Lorem ipsum dolor sit amet.</p> */}
 
@@ -87,20 +85,34 @@ const DonationView = async () => {
                 className="w-full"
               ></progress>
 
-              <div className={styles.main__infoBtn}>
-                <button className={styles.main__day}>
+              <div className={styles.main__chip_container}>
+                <button className={styles.main__chip}>
                   <Clock height={10} />
                   {val.attributes.daysRemaining} Days
                 </button>
-                <button className={styles.main__person}>
+                <button className={styles.main__chip}>
                   <People height={10} />
                   {val.attributes.backers} bakers
                 </button>
               </div>
             </div>
             <div className={styles.main__btns}>
-              <button className={styles.main__Share}>Share</button>
-              <button className={styles.main__Donate}>Donate Now</button>
+              <Button
+                href=""
+                as={Link}
+                variant={"secondary2"}
+                className="w-full py-3 px-0 text-sm2 border border-[#F5D9B1]"
+              >
+                Share
+              </Button>
+              <Button
+                href=""
+                as={Link}
+                variant={"primary"}
+                className="w-full py-3 px-0 text-sm2"
+              >
+                Donate Now
+              </Button>
             </div>
           </div>
         );
